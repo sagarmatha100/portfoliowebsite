ec849f7bd84202936751bc0f1aa62572a623278d
sagarmatha100 <83895535+sagarmatha100@users.noreply.github.com>
2023-06-24 13:51:14 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["n Engineering Student","Blogger", "Freelancer"],
+        strings: ["Civil Engineer","Blogger", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
